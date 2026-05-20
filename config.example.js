984a3eb8832b04225a50cb7405ad080bebd5dbac
Valentina Rachiele <valentina.rachiele@isiaurbino.net>
2026-05-20 10:03:55 +0200

    Risoluzione problema API key visibile

---
+++ config.example.js
@@ -0,0 +1,8 @@
+// config.example.js
+// Rinomina questo file in 'config.js' e inserisci le tue chiavi API di Google.
+// IMPORTANTE: Non caricare il file 'config.js' reale su GitHub!
+const CONFIG = {
+  CLIENT_ID: 'INSERISCI_QUI_IL_TUO_CLIENT_ID',
+  API_KEY: 'INSERISCI_QUI_LA_TUA_API_KEY',
+  MY_KEY: 'INSERISCI_QUI_LA_TUA_API_KEY'
+};
